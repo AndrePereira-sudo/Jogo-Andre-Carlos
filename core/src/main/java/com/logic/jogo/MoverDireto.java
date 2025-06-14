30b6b5a73f54fe8156c59d4b165ae53b9081748d
Carlosmsp <112399387+Carlosmsp@users.noreply.github.com>
2025-06-14 11:27:39 +0100

    implementação interface
    implementação interface para Estratégia dos movimentos dos inimigos no  jogo ficando mais flexível e escalável, permitindo que cada inimigo use uma "estratégia" diferente para se mover.

---
+++ core/src/main/java/com/logic/jogo/MoverDireto.java
@@ -0,0 +1,37 @@
+//package com.logic.jogo;
+package com.mygdx.game.inimigos;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class MoverDireto implements com.mygdx.game.inimigos.MovimentoStrategy {
+    private float velocidade;
+
+    public MoverDireto(float velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    @Override
+    public void mover(Rectangle inimigo, Rectangle jogador) {
+        // Calcula a direção do movimento do inimigo em direcção ao jogador
+            if (inimigo.x < jogador.x) inimigo.x += velocidade;
+            if (inimigo.x > jogador.x) inimigo.x -= velocidade;
+            if (inimigo.y < jogador.y) inimigo.y += velocidade;
+            if (inimigo.y > jogador.y) inimigo.y -= velocidade;
+
+/*        // Alternativa: Move o inimigo diretamente na direção do jogador
+        float deltaX = jogador.x - inimigo.x;
+        float deltaY = jogador.y - inimigo.y;
+        float distancia = (float) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        // Normaliza a direção
+        if (distancia > 0) {
+            deltaX /= distancia;
+            deltaY /= distancia;
+        }
+
+        // Move o inimigo na direção do jogador
+        inimigo.x += deltaX * velocidade;
+        inimigo.y += deltaY * velocidade;
+       */
+    }
+}
---
+++ core/src/main/java/com/logic/jogo/MovimentoStrategy.java
@@ -0,0 +1,12 @@
+//package com.logic.jogo;
+package com.mygdx.game.inimigos;
+import com.badlogic.gdx.math.Rectangle;
+import java.awt.*;
+/*
+ * Interface que define a estratégia de movimento dos inimigos no jogo.
+  * específica para diferentes tipos de inimigos.
+ */
+public interface MovimentoStrategy {
+    void mover(Rectangle inimigo, Rectangle jogador);
+
+}
